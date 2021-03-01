f7146069df83094ed35e7d5191db52fbdd797e68
dries-melis <dries.melis@melweb.org>
2021-03-01 22:18:08 +0100

    commitje

--- pokeback/app.js
+++ pokeback/app.js
@@ -2,19 +2,21 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose')
 
+//import routes
+const postsRoute = require('./routes/posts')
+
+app.use('/posts', postsRoute)
 
 //ROUTES
 app.get('/', (req, res) => {
     res.send('we are home');
 })
-app.get('/posts', (req, res) => {
-    res.send('we are posts');
-})
+
 
 
 
 //Connect to db
-
+mongoose.connect('mongodb://localhost:27017/pokinfo', { useNewUrlParser: true }, () => console.log('connected to DB'))
 
 //start listening
 app.listen(3000)
